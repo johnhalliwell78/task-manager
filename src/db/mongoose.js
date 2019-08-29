2d008ef186a5d232b8d82807ad9c001e25d18e65
Giang Tran Hoang <giangthse62424@fpt.edu.vn>
2019-08-29 14:06:26 +0700

    commit NodeJS Express Project

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 
---
+++ src/routers/task.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const Task = require('../models/task');
+const router = express.Router();
+
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    try {
+        await task.save();
+        res.status(201).send();
+    } catch (e) {
+        res.status(500).send(e);
+    }
+
+    // task.save().then(() => {
+    //     res.status(201).send(task);
+    // }).catch((error) => {
+    //     res.status(400).send(error);
+    // });
+});
+
+router.get('/tasks', async (req, res) => {
+
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks);
+    // }).catch((error) => {
+    //     res.status(500).send(error);
+    // });
+});
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //         return res.status(404).send();
+    //     }
+    //     res.send(task);
+    // }).catch((error) => {
+    //     res.status(500).send(error);
+    // });
+});
+
+
+router.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    });
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'invalid operation'});
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+router.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const task = await Task.findByIdAndDelete(_id);
+        if (!task) {
+            res.status(404).send();
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+module.exports = router;
